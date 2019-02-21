388ed39620d9ad9f2ad2ba05417dc348474b04fa
stat200 <bilyuga26@yandex.ru>
2019-02-21 16:36:38 +0300

    get recomendations

--- console/services/RecomendationService.php
+++ console/services/RecomendationService.php
@@ -1,7 +1,26 @@
 <?php
 namespace console\services;
 
+use console\helpers\RecomendationHelper;
+
 class RecomendationService
 {
-
+    /**
+     * @param array $usersFriendsList
+     * @return array
+     */
+    public function getRecomendationFriendList(array $usersFriendsList): array
+    {
+        $usersFriendsList = RecomendationHelper::prepareUserFriendsArray($usersFriendsList);
+        $recomendations = [];
+        foreach ($usersFriendsList as $key => $item) {
+            foreach ($usersFriendsList as $sub_key => $sub_item) {
+                if (!isset($recomendations[$key][$sub_key]) && $key != $sub_key && ($weight = RecomendationHelper::getSimilarityIndex($item, $sub_item))) {
+                    $recomendations[$key][$sub_key] = $weight;
+                    $recomendations[$sub_key][$key] = $weight;
+                }
+            }
+        }
+        return $recomendations;
+    }
 }
